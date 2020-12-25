f5c0fbf2c946ef7fefbce9fa953803f8a03a9059
Kateryna-Urbanovych <katya_urb@ukr.net>
2020-12-25 18:58:57 +0200

    добавляет стартовые файлы

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// const Status = {
+//     IDLE: 'idle',
+//     PENDING: 'pending',
+//     RESOLVED: 'resolved',
+//     REJECTED: 'rejected',
+// };
+
+export default function App() {
+    return (
+        <>
+            <h1>App</h1>
+        </>
+    );
+}
