c2d012a3a791d9f91f7ad3a6a284abbd190facab
Kateryna-Urbanovych <katya_urb@ukr.net>
2021-01-01 23:34:46 +0200

    добавляет history и location

--- src/views/MoviesPage/MoviesPage.js
+++ src/views/MoviesPage/MoviesPage.js
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import { Link, useRouteMatch } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { Link, useRouteMatch, useHistory, useLocation } from 'react-router-dom';
 import * as theMovieDbAPI from '../../servises/themoviedb-api';
 
 export default function MoviesPage() {
@@ -9,26 +9,37 @@ export default function MoviesPage() {
     const { url } = useRouteMatch();
     // console.log(url);
 
+    const history = useHistory();
+    const location = useLocation();
+    const searchMovie = new URLSearchParams(location.search).get('query') ?? '';
+
     const handleMovieQueryChange = ({ target }) => {
-        // console.log(target.value);
         setMovieQuery(target.value.toLowerCase());
     };
 
     const handleSubmit = event => {
         event.preventDefault();
 
-        // По превью дз запрос удаляется!!!
-        setMovieQuery('');
-
         if (movieQuery.trim() === '') {
             return window.alert('Please, write some request');
             // return toast.info('Please, write some request');
         }
 
-        theMovieDbAPI
-            .fetchMovieByName(movieQuery)
-            .then(({ results }) => setMovies(results));
+        history.push({ ...location, search: `query=${movieQuery}` });
+
+        // По превью дз запрос удаляется!!!
+        setMovieQuery('');
     };
+
+    useEffect(() => {
+        if (searchMovie === '') {
+            return;
+        }
+
+        theMovieDbAPI
+            .fetchMovieByName(searchMovie)
+            .then(({ results }) => setMovies(results));
+    }, [searchMovie]);
 
     return (
         <>
